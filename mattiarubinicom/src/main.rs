7ddeb7afe8a40e1fc8693228b1d3c705b21352f9
Mattia Rubini <mattia.rubini@gmail.com>
2021-12-22 16:27:33 +0100

    Fixed lanfing page, added about me page

--- mattiarubinicom/src/main.rs
+++ mattiarubinicom/src/main.rs
@@ -10,11 +10,18 @@ fn index() -> Template {
     Template::render("pages/landing_page", context)
 }
 
+#[get("/about-me")]
+fn aboutme() -> Template {
+    let context: HashMap<String, String> = HashMap::new();
+    Template::render("pages/about_me", context)
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         // Mounting routes
         .mount("/", routes![index])
+        .mount("/", routes![aboutme])
         // Serving static files
         .mount("/css", FileServer::from("./static/css"))
         .mount("/images", FileServer::from("./static/images"))
